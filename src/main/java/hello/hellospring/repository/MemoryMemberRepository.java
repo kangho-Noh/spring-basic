20040ead752ab3deddd46fcba4bb0a773dedbe4a
kangho <rkdgh98@khu.ac.kr>
2021-08-11 20:44:57 +0900

    Try JUnit Test, Add sign up, and etc service

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -35,6 +35,11 @@ public class MemoryMemberRepository implements MemberRepository{
 
     @Override
     public List<Member> findAll() {
+
         return new ArrayList<>(store.values());
+    }
+
+    public void clearStore(){
+        store.clear();
     }
 }
