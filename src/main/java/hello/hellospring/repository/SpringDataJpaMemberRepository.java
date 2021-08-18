f6da3afdb81b753486bdb982906ef98e2d04d2e9
Kangho-Noh <rkdgh98@khu.ac.kr>
2021-08-19 00:15:56 +0900

    Add comments

--- src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
+++ src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -7,6 +7,8 @@ import java.util.Optional;
 
 public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
 
+    //JPQL select m from Member m where m.name = ?
     @Override
     Optional<Member> findByName(String name);
+    //인터페이스 규칙을 지켜서 인터페이스를 만드는 것 만으로도 개발이 끝나버림
 }
